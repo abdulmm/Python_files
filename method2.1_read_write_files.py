8a4d49368f7d86c06acb59b5799d81aa1ffa62cb
abdulmm <51336587+abdulmm@users.noreply.github.com>
2019-06-15 21:09:51 -0500

    Add files via upload

---
+++ method2.1_read_write_files.py
@@ -0,0 +1,38 @@
+#Mohammed Abdul Mohi
+
+import sys
+
+if len(sys.argv) < 2:
+	print('The correct usage of this file is: python method2_read_write_files.py <file_text.txt>')
+	exit(1)
+	
+filename = sys.argv[1]
+f = open(filename,'r')
+
+data = f.read()
+f.close()
+
+def lines_count(data):
+	lines = data.split('\n')
+	for line in lines:
+		if not line:
+			lines.remove(line)
+	num_lines = len(lines)
+	return num_lines
+	
+def words_count(data):
+	words = []
+	lines = data.split('\n')
+	for line in lines:
+		if not line:
+			lines.remove(line)
+		else:
+			line_words = line.split(' ')
+			for word in line_words:
+				words.append(word)
+	num_words = len(words)
+	return num_words
+	
+print('\n')
+print('Number of lines = ',lines_count(data))
+print('Number of words = ',words_count(data))								
